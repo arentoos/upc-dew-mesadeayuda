97b672bd826bc98c5b656fccaf27702dfebe095b
carlos.zegarra@gmail.com <carlos.zegarra@gmail.com@ce62a7ff-e9c6-03d1-ee30-77284e50f473>
2010-09-22 15:53:59 +0000

    revisión de entidades después de reunión de coordinación ayer

--- trunk/helpdesk/src/main/java/pe/edu/upc/dew/helpdesk/model/Comentario.java
+++ trunk/helpdesk/src/main/java/pe/edu/upc/dew/helpdesk/model/Comentario.java
@@ -5,68 +5,67 @@ package pe.edu.upc.dew.helpdesk.model;
 // </editor-fold> 
 public class Comentario {
 
-        // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
     // #[regen=yes,id=DCE.41A423A2-1696-063E-37C6-77C66195C206]
     // </editor-fold>
-    private int id_comentario;
+    private String texto;
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.41A423A2-1696-063E-37C6-77C66195C206]
-    // </editor-fold> 
-    private String comentario;
-
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
     // #[regen=yes,id=DCE.BC92BFBF-4757-6B81-8F54-EC4A1640C333]
-    // </editor-fold> 
-    private String fechahora;
+    // </editor-fold>
+    private String fechaHora;
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
+    // #[regen=yes,id=DCE.0B01B931-2AEF-4058-AAF8-19CBD9E174E5]
+    // </editor-fold>
+    private int idComentario;
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
     // #[regen=yes,id=DCE.ADCE2D28-AB64-A5B7-AC97-0AD2C3A61EDC]
-    // </editor-fold> 
+    // </editor-fold>
     public Comentario () {
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,regenBody=yes,id=DCE.D74570C1-7D32-4C71-D96C-EC034C9CE5F5]
-    // </editor-fold> 
-    public String getComentario () {
-        return comentario;
-    }
-
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,regenBody=yes,id=DCE.8E9E3988-60C5-960A-1087-FA45E0144CD2]
-    // </editor-fold> 
-    public void setComentario (String val) {
-        this.comentario = val;
-    }
-
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
     // #[regen=yes,regenBody=yes,id=DCE.E0DFF767-D54F-7200-29C1-924CC528CB19]
-    // </editor-fold> 
-    public String getFechahora () {
-        return fechahora;
+    // </editor-fold>
+    public String getFechaHora () {
+        return fechaHora;
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
     // #[regen=yes,regenBody=yes,id=DCE.95F6CC70-931E-2E8D-3C48-990A9B111F97]
-    // </editor-fold> 
-    public void setFechahora (String val) {
-        this.fechahora = val;
+    // </editor-fold>
+    public void setFechaHora (String val) {
+        this.fechaHora = val;
     }
 
-    /**
-     * @return the id_comentario
-     */
-    public int getId_comentario() {
-        return id_comentario;
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
+    // #[regen=yes,regenBody=yes,id=DCE.16C09DFD-43E6-D8F3-0DEC-74139EB2EB15]
+    // </editor-fold>
+    public int getIdComentario () {
+        return idComentario;
     }
 
-    /**
-     * @param id_comentario the id_comentario to set
-     */
-    public void setId_comentario(int id_comentario) {
-        this.id_comentario = id_comentario;
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
+    // #[regen=yes,regenBody=yes,id=DCE.2E57A8F0-A5A8-8549-1A5D-14D96150B42E]
+    // </editor-fold>
+    public void setIdComentario (int val) {
+        this.idComentario = val;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
+    // #[regen=yes,regenBody=yes,id=DCE.D74570C1-7D32-4C71-D96C-EC034C9CE5F5]
+    // </editor-fold>
+    public String getTexto () {
+        return texto;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
+    // #[regen=yes,regenBody=yes,id=DCE.8E9E3988-60C5-960A-1087-FA45E0144CD2]
+    // </editor-fold>
+    public void setTexto (String val) {
+        this.texto = val;
     }
 
 }
-
--- trunk/helpdesk/src/main/java/pe/edu/upc/dew/helpdesk/model/Encuesta.java
+++ trunk/helpdesk/src/main/java/pe/edu/upc/dew/helpdesk/model/Encuesta.java
@@ -5,69 +5,124 @@ package pe.edu.upc.dew.helpdesk.model;
 // </editor-fold> 
 public class Encuesta {
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
     // #[regen=yes,id=DCE.D5CBD58C-3923-83AE-17FE-42FCE1803EE5]
-    // </editor-fold> 
-    private int id_encuesta;
+    // </editor-fold>
+    private int idEncuesta;
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
     // #[regen=yes,id=DCE.FEC90557-2389-F0B0-1F33-CA3636BE6080]
-    // </editor-fold> 
-    private int resultado;
+    // </editor-fold>
+    private int servicio;
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
     // #[regen=yes,id=DCE.29EA6E54-8EF0-58D8-3899-5CC41FDECDDC]
-    // </editor-fold> 
+    // </editor-fold>
+    private int tiempo;
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
+    // #[regen=yes,id=DCE.696D650F-C987-AE21-72E1-ECFAE93A3C9D]
+    // </editor-fold>
+    private int trato;
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
+    // #[regen=yes,id=DCE.CE12E4E8-8C38-289D-1469-971CFA084618]
+    // </editor-fold>
+    private int conocimiento;
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
+    // #[regen=yes,id=DCE.DF52C8CB-9711-D105-C300-0F19FF813063]
+    // </editor-fold>
     private String comentario;
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
     // #[regen=yes,id=DCE.10893A8A-31E5-57F4-9D84-53449485AE25]
-    // </editor-fold> 
+    // </editor-fold>
     public Encuesta () {
     }
 
-   
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,regenBody=yes,id=DCE.122B4B1A-9C09-A7BC-9C8A-A40DA14EF33A]
-    // </editor-fold> 
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
+    // #[regen=yes,regenBody=yes,id=DCE.CB911D79-9BA2-910B-6AA0-5687FBCE160E]
+    // </editor-fold>
     public String getComentario () {
         return comentario;
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,regenBody=yes,id=DCE.94882370-D563-3174-790C-50A23C267D7A]
-    // </editor-fold> 
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
+    // #[regen=yes,regenBody=yes,id=DCE.3768843D-2A40-9FF7-C208-2D23EBFB7416]
+    // </editor-fold>
     public void setComentario (String val) {
         this.comentario = val;
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
+    // #[regen=yes,regenBody=yes,id=DCE.06A3519B-5C2E-7649-7603-0253585AF0BE]
+    // </editor-fold>
+    public int getConocimiento () {
+        return conocimiento;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
+    // #[regen=yes,regenBody=yes,id=DCE.1E917A93-2ED0-8794-B7D2-9C3B89AE9E00]
+    // </editor-fold>
+    public void setConocimiento (int val) {
+        this.conocimiento = val;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
+    // #[regen=yes,regenBody=yes,id=DCE.606F32B7-B812-6343-9D01-56D6780AC110]
+    // </editor-fold>
+    public int getIdEncuesta () {
+        return idEncuesta;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
+    // #[regen=yes,regenBody=yes,id=DCE.119707DD-608D-2A2B-9D12-D62FE3632EF4]
+    // </editor-fold>
+    public void setIdEncuesta (int val) {
+        this.idEncuesta = val;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
     // #[regen=yes,regenBody=yes,id=DCE.46988D38-CD03-5F0B-DBF2-1DE88A7246F3]
-    // </editor-fold> 
-    public int getResultado () {
-        return resultado;
+    // </editor-fold>
+    public int getServicio () {
+        return servicio;
     }
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
     // #[regen=yes,regenBody=yes,id=DCE.2063B349-4F7A-6D7F-E77D-6917412615CC]
-    // </editor-fold> 
-    public void setResultado (int val) {
-        this.resultado = val;
+    // </editor-fold>
+    public void setServicio (int val) {
+        this.servicio = val;
     }
 
-    /**
-     * @return the id_encuesta
-     */
-    public int getId_encuesta() {
-        return id_encuesta;
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
+    // #[regen=yes,regenBody=yes,id=DCE.122B4B1A-9C09-A7BC-9C8A-A40DA14EF33A]
+    // </editor-fold>
+    public int getTiempo () {
+        return tiempo;
     }
 
-    /**
-     * @param id_encuesta the id_encuesta to set
-     */
-    public void setId_encuesta(int id_encuesta) {
-        this.id_encuesta = id_encuesta;
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
+    // #[regen=yes,regenBody=yes,id=DCE.94882370-D563-3174-790C-50A23C267D7A]
+    // </editor-fold>
+    public void setTiempo (int val) {
+        this.tiempo = val;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
+    // #[regen=yes,regenBody=yes,id=DCE.C3B649CD-C5E4-6697-5BA8-DAF343311F0E]
+    // </editor-fold>
+    public int getTrato () {
+        return trato;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker ">
+    // #[regen=yes,regenBody=yes,id=DCE.F85C6619-34F8-A640-3A00-52DFEA5BAE86]
+    // </editor-fold>
+    public void setTrato (int val) {
+        this.trato = val;
     }
 
 }
-
