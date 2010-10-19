6938b6a640025d820e2bb4853bb424e47d9feacd
carlos.zegarra@gmail.com <carlos.zegarra@gmail.com@ce62a7ff-e9c6-03d1-ee30-77284e50f473>
2010-10-19 22:03:50 +0000

    Inicio - pantalla de login 17:00 (19-Oct)

--- trunk/helpdesk/src/main/java/pe/edu/upc/dew/helpdesk/controller/LoginServlet.java
+++ trunk/helpdesk/src/main/java/pe/edu/upc/dew/helpdesk/controller/LoginServlet.java
@@ -54,4 +54,10 @@ public class LoginServlet extends HttpServlet {
         }
 
     }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.getRequestDispatcher("logeo_empleado.jsp").forward(req, resp);
+    }
 }
