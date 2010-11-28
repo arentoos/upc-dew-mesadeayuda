f4f300eb571e612e1ddb904812140e4a70e08ca5
carlos.zegarra@gmail.com <carlos.zegarra@gmail.com@ce62a7ff-e9c6-03d1-ee30-77284e50f473>
2010-11-28 23:57:03 +0000

    Conexion a la BD

---
+++ trunk/helpdesk/src/main/java/pe/edu/upc/dew/helpdesk/dao/EmpleadoDao.java
@@ -0,0 +1,8 @@
+package pe.edu.upc.dew.helpdesk.dao;
+
+import pe.edu.upc.dew.helpdesk.model.Empleado;
+
+public interface EmpleadoDao {
+
+    public Empleado getEmpleadoPorLoginName(String loginName);
+}
---
+++ trunk/helpdesk/src/main/java/pe/edu/upc/dew/helpdesk/dao/EmpleadoDaoJdbc.java
@@ -0,0 +1,39 @@
+package pe.edu.upc.dew.helpdesk.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import pe.edu.upc.dew.helpdesk.model.Empleado;
+import pe.edu.upc.dew.helpdesk.util.JdbcUtils;
+
+public class EmpleadoDaoJdbc implements EmpleadoDao {
+
+    public Empleado getEmpleadoPorLoginName(String loginName) {
+
+        Connection conn = JdbcUtils.getConnection();
+
+        try {
+            Statement st = conn.createStatement();
+
+            ResultSet rs = st.executeQuery("select login, clave, nombre from Empleado where login ='" + loginName + "'");
+
+            if (rs.next()) {
+                Empleado empleado = new Empleado();
+                // nombre de la columna
+                empleado.setLogin(rs.getString("login"));
+                empleado.setClave(rs.getString("clave"));
+                empleado.setNombre(rs.getString("nombre"));
+                return empleado;
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(EmpleadoDaoJdbc.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+
+}
