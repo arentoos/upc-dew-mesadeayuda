23dce862671cba4d42f1117575aae26d9e7debbc
cristigonzagah <cristigonzagah@ce62a7ff-e9c6-03d1-ee30-77284e50f473>
2010-12-05 03:28:15 +0000

    Ultimas modificaciones para Detalle Ticket (comentario) 10:26

---
+++ helpdesk/src/main/java/pe/edu/upc/dew/helpdesk/dao/ComentarioDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.edu.upc.dew.helpdesk.dao;
+
+/**
+ *
+ * @author cgonzagah
+ */
+public interface ComentarioDao {
+
+
+    public void insertComentario(String string);
+
+}
---
+++ helpdesk/src/main/java/pe/edu/upc/dew/helpdesk/dao/ComentarioDaoJdbc.java
@@ -0,0 +1,34 @@
+package pe.edu.upc.dew.helpdesk.dao;
+
+import java.sql.Connection;
+//import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+//import pe.edu.upc.dew.helpdesk.model.Empleado;
+import pe.edu.upc.dew.helpdesk.util.JdbcUtils;
+
+/**
+ *
+ * @author cgonzagah
+ */
+
+
+public class ComentarioDaoJdbc implements ComentarioDao {
+
+    public void insertComentario(String string) {
+
+        Connection conn = JdbcUtils.getConnection();
+
+        try {
+            Statement st = conn.createStatement();
+
+            String query = "INSERT INTO Comentario (fecha, comentario, idTicket, idEmpleado)" + "VALUES (" + string + ")";
+
+            st.executeUpdate(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(EmpleadoDaoJdbc.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
