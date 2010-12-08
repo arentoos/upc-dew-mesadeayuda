fc4d22fa84899bbded1e0eff8ec0b6d9e61974cd
raulneyra <raulneyra@ce62a7ff-e9c6-03d1-ee30-77284e50f473>
2010-12-08 20:47:38 +0000

    22.39

--- trunk/helpdesk/src/main/java/pe/edu/upc/dew/helpdesk/dao/ComentarioDao.java
+++ trunk/helpdesk/src/main/java/pe/edu/upc/dew/helpdesk/dao/ComentarioDao.java
@@ -2,8 +2,10 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pe.edu.upc.dew.helpdesk.dao;
+
+import java.util.ArrayList;
+import pe.edu.upc.dew.helpdesk.model.Comentario;
 
 /**
  *
@@ -11,7 +13,8 @@ package pe.edu.upc.dew.helpdesk.dao;
  */
 public interface ComentarioDao {
 
-
     public void insertComentario(String string);
+
+    public ArrayList<Comentario> getComentarios(String idTicket);
 
 }
